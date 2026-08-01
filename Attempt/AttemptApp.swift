469a102626e964402bcff9a750ca30d743a4c6fe
Oleksii Kotsiuba <aleksey.kotsuba@gmail.com>
2026-08-01 16:03:11 -0400

    Initial Commit

---
+++ Attempt/AttemptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttemptApp.swift
+//  Attempt
+//
+//  Created by lockw1n on 01.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct AttemptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
